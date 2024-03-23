114c5cfedd638a813f4894c67c14228427e2fc24
Owhenthesaints <owen.simon@epfl.ch>
2024-03-23 19:12:21 +0100

    modified pio + simple serial print test

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,10 @@
 int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+	Serial.begin(9600);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+	Serial.println(1);
 }
