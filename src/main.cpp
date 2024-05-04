d96719ea26af9f60ae150e5361fa290eb10a65cc
Owhenthesaints <owen.simon@epfl.ch>
2024-05-04 23:40:40 +0200

    simple test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,145 +1,16 @@
 #include <Arduino.h>
-#include <Ultrasonic.h>
-#include <math.h>
-#define HC_PIN_0_0 2
-#define HC_PIN_0_1 4
-#define HC_PIN_1_0 7
-#define HC_PIN_1_1 8
-#define HC_PIN_2_0 9
-#define HC_PIN_2_1 10
-#define HC_PIN_3_0 11
-#define HC_PIN_3_1 12
-#define HC_PIN_4_0 13
-#define HC_PIN_4_1 6
-#define OOB(a) ((a)>200?0:(a))//Out of bounds returns 0 if out of bounds
-#define UINT_DIV_2(a) ((a)>>1)
-#define NUM_DIST_SENS 5
-#define PWM_PIN_1 3 // Predefined PWM output pin
-#define PWM_PIN_2 5
-#define ENABLE_PIN_1 A0 // Predefined enable pins
-#define ENABLE_PIN_2 A1
-#define DIRECTION_PIN_1 A2 // Predefined pin for controlling direction
-#define DIRECTION_PIN_2 A3
-#define STOP_CHAR_RX 101 // stop chars
-#define STOP_CHAR_TX 201
-#define LIST_SIZE 3 // incoming list sizes
-#define TO_PWM_CONV 2.5
 
-int8_t buffer[LIST_SIZE]; // String to store incoming data
+#define STOP_CHAR 101
+uint8_t i = 0;
 
-
-Ultrasonic distanceSensor0(HC_PIN_0_0, HC_PIN_0_1); 
-Ultrasonic distanceSensor1(HC_PIN_1_0, HC_PIN_1_1);
-Ultrasonic distanceSensor2(HC_PIN_2_0, HC_PIN_2_1);
-Ultrasonic distanceSensor3(HC_PIN_3_0, HC_PIN_3_1);
-Ultrasonic distanceSensor4(HC_PIN_4_0, HC_PIN_4_1);
-
-void initMotors()
-{
-  	digitalWrite(ENABLE_PIN_1, LOW);
-  	digitalWrite(ENABLE_PIN_2, LOW);
-  	analogWrite(PWM_PIN_1, 0);
-  	analogWrite(PWM_PIN_2, 0);
-  	digitalWrite(ENABLE_PIN_1, HIGH);
-  	digitalWrite(ENABLE_PIN_2, HIGH);
+void setup() {
+    Serial.begin(9600);
 }
 
-void setup()
-{
-	pinMode(PWM_PIN_1, OUTPUT);
-  	pinMode(ENABLE_PIN_1, OUTPUT);
-	pinMode(DIRECTION_PIN_1, OUTPUT);
-	pinMode(PWM_PIN_2, OUTPUT);
-  	pinMode(ENABLE_PIN_2, OUTPUT);
-	pinMode(DIRECTION_PIN_2, OUTPUT);
-  	initMotors();
-	Serial.begin(9600); // Initialize serial communication
-}
-
-void writeToMotor(bool left, int8_t inputValue)
-{
-	// Calculate absolute value of input
-	int8_t absValue = abs(inputValue);
-
-	// Determine direction
-  	digitalWrite(left ? DIRECTION_PIN_1 : DIRECTION_PIN_2, inputValue < 0 ? HIGH : LOW);
-
-	absValue *=static_cast<int8_t>(floor(inputValue * TO_PWM_CONV));
-
-	// Output PWM value
-	analogWrite(left ? PWM_PIN_1 : PWM_PIN_2, absValue);
-}
-
-bool processBuffer()
-{
-	// Extract last two characters separated by STOP_CHAR
-	// Extract last two characters
-	uint8_t index;
-	bool found = false;
-	for (uint8_t i =0; i<LIST_SIZE; i++){
-		if (buffer[i]==STOP_CHAR_RX){
-			index = i;
-			found = true;
-			continue;
-		}
-	}
-
-	if (found)
-	{
-		switch (index)
-		{
-		case 0:
-			writeToMotor(true, buffer[1]);
-			writeToMotor(false, buffer[2]);
-			break;
-		case 1:
-			writeToMotor(false, buffer[0]);
-			writeToMotor(true, buffer[2]);
-			break;
-		case 2:
-			writeToMotor(true, buffer[0]);
-			writeToMotor(false, buffer[1]);
-			break;
-		default:
-			Serial.println("inv");
-			break;
-		}
-	}
-	return found;
-}
-
-uint8_t * readSensors(){
-	uint8_t *sensorArray = new uint8_t[NUM_DIST_SENS];
-	uint8_t sensorValue0 = UINT_DIV_2(distanceSensor0.read(CM)); // divides the number by 2
-	uint8_t sensorValue1 = UINT_DIV_2(distanceSensor1.read(CM));
-	uint8_t sensorValue2 = UINT_DIV_2(distanceSensor2.read(CM));
-	uint8_t sensorValue3 = UINT_DIV_2(distanceSensor3.read(CM));
-	uint8_t sensorValue4 = UINT_DIV_2(distanceSensor4.read(CM));
-	sensorArray[0] = OOB(sensorValue0); //cuts off the number at 200
-	sensorArray[1] = OOB(sensorValue1);
-	sensorArray[2] = OOB(sensorValue2);
-	sensorArray[3] = OOB(sensorValue3);
-	sensorArray[4] = OOB(sensorValue4);
-	return sensorArray;
-}
-
-
-void loop()
-{
-	while (Serial.available()>3){
-		Serial.read();
-	}
-	uint8_t incrementalPointer = 0;
-	while (incrementalPointer <= 2)
-	{
-		if (Serial.available()){
-			char incomingChar = Serial.read();
-			buffer[incrementalPointer] = int8_t(incomingChar);
-			incrementalPointer++;
-		}
-	}
-	if (incrementalPointer==3)
-	{
-		processBuffer();
-	}
+void loop() {
+    for (size_t j = 0; j < 5; j++) {
+        i++;
+        Serial.print(char(i));
+    }
+    Serial.print(char(STOP_CHAR));
 }
