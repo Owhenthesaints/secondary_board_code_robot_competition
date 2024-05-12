e4356ea6e19e98f132c359428ffc3fa3c3eca09d
Owhenthesaints <owen.simon@epfl.ch>
2024-05-13 00:09:17 +0200

    working clock code

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,21 @@
 #include <Arduino.h>
 
+
 #define STOP_CHAR 101
 #define LIST_SIZE 3
 #define NUM_DIST_SENSORS 5
 int8_t buffer[LIST_SIZE];
+unsigned long intervalTime;
 
 void setup() {
     Serial.begin(9600);
+	intervalTime = millis();
 }
 
-void loop()
-{
-	while (Serial.available() > 3)
-	{
-		Serial.read();
+void loop() {
+	if (millis() - intervalTime > 3000){
+		Serial.println("three sec");
+		intervalTime = millis();
 	}
-	uint8_t incremental_pointer(0);
-	while (incremental_pointer <= 2)
-	{
-		if (Serial.available())
-		{
-			char incommingChar = Serial.read();
-			buffer[incremental_pointer] = int8_t(incommingChar);
-			incremental_pointer++;
-		}
-	}
-	for (size_t i(0); i < NUM_DIST_SENSORS; i++){
-		Serial.print(char(buffer[i%2]));
-	}
-	Serial.print(char(STOP_CHAR));
+
 }
