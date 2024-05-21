8d7e56f04421a21acaaf7f90b2de042df7fa6de0
Owhenthesaints <owen.simon@epfl.ch>
2024-05-21 13:44:03 +0200

    semi-working distance

--- src/main.cpp
+++ src/main.cpp
@@ -1,241 +1,58 @@
 #include <Arduino.h>
-#include <math.h>
-#define HC_PIN_0_0 30
-#define HC_PIN_0_1 31
-#define HC_PIN_1_0 32
-#define HC_PIN_1_1 33
-#define HC_PIN_2_0 34
-#define HC_PIN_2_1 35
-#define HC_PIN_3_0 36
-#define HC_PIN_3_1 37
-#define HC_PIN_4_0 38
-#define HC_PIN_4_1 39
-#define OOB(a) ((a)>200?0:(a))//Out of bounds returns 0 if out of bounds
-#define UINT_DIV_2(a) ((a)>>1)
-#define NUM_DIST_SENS 5
-#define PWM_PIN_1 2 // Predefined PWM output pin
-#define PWM_PIN_2 3
-#define ENABLE_PIN_1 22
-#define ENABLE_PIN_2 24
-#define DIRECTION_PIN_1 23 // Predefined pin for controlling direction
-#define DIRECTION_PIN_2 25
-#define STOP_CHAR_RX 101 // Stop chars
-#define STOP_CHAR_TX 101
-#define LIST_SIZE 3 // RX list size
-#define TO_PWM_CONST 2.5 // PWM conversion constant
-#define MIN_PWM 70
-#define NUM_DIST_SENSORS 5
-#define STOP_CHAR 101
-#define MAX_TIME 1000
+const int numSensors = 5;
 
-int8_t buffer[LIST_SIZE]; // String to store incoming data
+const int triggerPins[numSensors] = {30, 32, 34, 36, 38}; // Broches pour les triggers, modifiables
+const int echoPins[numSensors] = {2, 3, 18, 19, 20}; // Broches pour les echos, doivent être compatibles avec les interruptions
 
-inline bool TimeExceeded(long int & intervalTime){
-	return static_cast<long int>(intervalTime + MAX_TIME - millis()) < 0;
+volatile long startTimes[numSensors];
+volatile long echoTimes[numSensors];
+volatile bool newMeasurementAvailable[numSensors];
+
+void setup() {
+  Serial.begin(9600);
+
+  for (int i = 0; i < numSensors; i++) {
+    pinMode(triggerPins[i], OUTPUT);
+    pinMode(echoPins[i], INPUT);
+    attachInterrupt(digitalPinToInterrupt(echoPins[i]), ISR_echo, CHANGE);
+    newMeasurementAvailable[i] = false;
+  }
 }
 
-void writeToMotor(bool left, int8_t inputValue)
-{
-	// Calculate absolute value of input
-	uint8_t absValue = abs(inputValue);
+void loop() {
+  for (int i = 0; i < numSensors; i++) {
+    if (!newMeasurementAvailable[i]) {
+      digitalWrite(triggerPins[i], LOW);
+      delayMicroseconds(2);
+      digitalWrite(triggerPins[i], HIGH);
+      delayMicroseconds(10);
+      digitalWrite(triggerPins[i], LOW);
+    }
+  }
 
-	// Determine direction
-  	digitalWrite(left ? DIRECTION_PIN_1 : DIRECTION_PIN_2, inputValue < 0 ? HIGH : LOW);
+  for (int i = 0; i < 2; i++) {
+    if (newMeasurementAvailable[i]) {
+      long duration = echoTimes[i] - startTimes[i];
+      float distance = duration * 0.034 / 2;
+      Serial.print("Sensor ");
+      Serial.print(i);
+      Serial.print(": ");
+      Serial.println(distance);
+      newMeasurementAvailable[i] = false;
+    }
+  }
 
-
-	#ifdef DEBUG
-	Serial.println(String(inputValue));
-	#endif
-	// Convert absolute char value to PWM value (0 to 100 mapped to 0 to 255)
-	absValue = static_cast<uint8_t>(floor(absValue*TO_PWM_CONST));
-
-	#ifdef DEBUG
-	Serial.println(String(absValue));
-	#endif
-
-	if (absValue<MIN_PWM){
-		digitalWrite(left ? ENABLE_PIN_1 : ENABLE_PIN_2, LOW);
-		absValue = MIN_PWM;
-	} else {
-		digitalWrite(left ? ENABLE_PIN_1 : ENABLE_PIN_2, HIGH);
-	}
-	
-	#ifdef DEBUG
-	Serial.print("l");
-	Serial.println(String(absValue));
-	#endif
-	// Output PWM value
-	analogWrite(left ? PWM_PIN_1 : PWM_PIN_2, absValue);
+  delay(100);
 }
 
-inline void stopMotors()
-{
-	writeToMotor(false, 0);
-	writeToMotor(true, 0);
+void ISR_echo() {
+  for (int i = 0; i < numSensors; i++) {
+    if (digitalRead(echoPins[i]) == HIGH) {
+      startTimes[i] = micros();
+    } else {
+      echoTimes[i] = micros();
+      newMeasurementAvailable[i] = true;
+    }
+  }
 }
 
-
-void initMotors()
-{
-  	digitalWrite(ENABLE_PIN_1, LOW);
-  	digitalWrite(ENABLE_PIN_2, LOW);
-	writeToMotor(true, 0);
-	writeToMotor(false, 0);
-	digitalWrite(ENABLE_PIN_1, HIGH);
-	digitalWrite(ENABLE_PIN_2, HIGH);
-	delay(500);
-	digitalWrite(ENABLE_PIN_1, LOW);
-	digitalWrite(ENABLE_PIN_2, LOW);
-}
-
-void motorSetup() {
-	pinMode(PWM_PIN_1, OUTPUT);
-  	pinMode(ENABLE_PIN_1, OUTPUT);
-	pinMode(DIRECTION_PIN_1, OUTPUT);
-	pinMode(PWM_PIN_2, OUTPUT);
-  	pinMode(ENABLE_PIN_2, OUTPUT);
-	pinMode(DIRECTION_PIN_2, OUTPUT);
-}
-
-void distanceSetup() {
-	pinMode(HC_PIN_0_0, OUTPUT);
-	pinMode(HC_PIN_0_1, INPUT);
-	pinMode(HC_PIN_1_0, OUTPUT);
-	pinMode(HC_PIN_1_1, INPUT);
-	pinMode(HC_PIN_2_0, OUTPUT);
-	pinMode(HC_PIN_2_1, INPUT);
-	pinMode(HC_PIN_3_0, OUTPUT);
-	pinMode(HC_PIN_3_1, INPUT);
-	pinMode(HC_PIN_4_0, OUTPUT);
-	pinMode(HC_PIN_4_1, INPUT);
-}
-
-
-void setup()
-{
-	motorSetup();
-	distanceSetup();
-  	initMotors();
-	Serial.begin(9600); // Initialize serial communication
-}
-
-void stopMotors()
-{
-	writeToMotor(false, 0);
-	writeToMotor(true, 0);
-}
-
-
-/**
- * @brief Processes the buffer tries to find position of the stop char and write two values to motor
- * @returns bool indicating if the stop char has been found
-*/
-bool processBuffer()
-{
-	// Extract last two characters separated by STOP_CHAR
-	// Extract last two characters
-	uint8_t index;
-	bool found = false;
-	// find stop char
-	for (uint8_t i =0; i<LIST_SIZE; i++){
-		if (buffer[i]==STOP_CHAR_RX){
-			index = i;
-			found = true;
-			continue;
-		}
-	}
-
-	if (found)
-	{
-		// handling the different cases of position of stop_char
-		switch (index)
-		{
-		case 0:
-			#ifdef DEBUG
-		 	Serial.println("case 0");
-			#endif
-			writeToMotor(true, buffer[1]);
-			writeToMotor(false, buffer[2]);
-			break;
-		case 1:
-			#ifdef DEBUG
-			Serial.println("case 1");
-			#endif
-			writeToMotor(false, buffer[0]);
-			writeToMotor(true, buffer[2]);
-			break;
-		case 2:
-			#ifdef DEBUG
-			Serial.println("case 2");
-			#endif
-			writeToMotor(true, buffer[0]);
-			writeToMotor(false, buffer[1]);
-			break;
-		default:
-			#ifdef DEBUG
-			Serial.println("inv");
-			#endif
-			break;
-		}
-	}
-	return found;
-}
-
-unsigned char readDistance(int triggerPin, int echoPin) {
-  digitalWrite(triggerPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(triggerPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(triggerPin, LOW);
-
-  long duration = pulseIn(echoPin, HIGH);
-  long distance = duration * 0.034 / 2; // Speed of sound is 34 cm/ms
-
-  return static_cast<unsigned char>(distance);
-}
-
-void printSensors(){
-  Serial.print(char(readDistance(HC_PIN_0_0, HC_PIN_0_1)));
-  Serial.print(char(readDistance(HC_PIN_1_0, HC_PIN_1_1)));
-  Serial.print(char(readDistance(HC_PIN_2_0, HC_PIN_2_1)));
-  Serial.print(char(readDistance(HC_PIN_3_0, HC_PIN_3_1)));
-  Serial.print(char(readDistance(HC_PIN_4_0, HC_PIN_4_1)));
-  Serial.print(char(STOP_CHAR_TX));
-}
-
-
-void loop()
-{
-	static long int intervalTime(millis());
-	static bool firstTime(true);
-	printSensors();
-	while (Serial.available() > 3)
-	{
-		Serial.read();
-
-	}
-	intervalTime = millis();
-	uint8_t incremental_pointer(0);
-	while (incremental_pointer <= 2)
-	{
-		if (TimeExceeded(intervalTime))
-		{
-			stopMotors();
-			break;
-		}
-		else if (Serial.available())
-		{
-			char incommingChar = Serial.read();
-			buffer[incremental_pointer] = int8_t(incommingChar);
-			incremental_pointer++;
-			firstTime = false;
-		}
-		else if (firstTime)
-		{
-			break;
-		}
-	}
-	if(incremental_pointer == 3){
-		processBuffer();
-	}
-}
